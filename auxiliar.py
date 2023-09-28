8b4b03b6c20088e73e51375bf812b6ef94e877a1
Guilherme <guilherme.neves20@hotmail.com>
2023-09-28 17:33:12 -0300

    Aula 01 - Fim

---
+++ auxiliar.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+
+time.sleep(5)
+print(pyautogui.position())
+
+#          codigo       marca        tipo  categoria  preco_unitario  custo               obs
+# 0    MOLO000251    Logitech       Mouse          1           25.95    6.5               NaN
+# 1    MOLO000192    Logitech       Mouse          2           19.95    5.0               NaN
+# 2    CAHA000251     Hashtag      Camisa          1           25.00   11.0               NaN
+# 3    CAHA000252     Hashtag      Camisa          2           25.00   11.0  Conferir estoque
+# 4    MOMU000111  Multilaser       Mouse          1           11.99    3.4               NaN
+
+# Point(x=557, y=359) -> Login Email
+
--- codigo.py
+++ codigo.py
@@ -1,7 +1,8 @@
 # Passo a passo do projeto
 
-# Importando a biblioteca
+# Importando as bibliotecas
 import pyautogui
+import time
 
 pyautogui.PAUSE = 0.5 # Pausa entre cada comando
 
@@ -10,29 +11,88 @@ pyautogui.PAUSE = 0.5 # Pausa entre cada comando
 # pyautogui.write -> escrever um texto 
 # pyautogui.press -> apertar uma tecla 
 # pyautogui.hotkey -> atalho(combiação de teclas)
-
+# pyautogui.scroll -> núm. positivo sobe(500) e núm. negativo desce(-500)
 
 # Passo 1: Entrar no sistema da empresa
   # https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
     # 1.1 Abrir o Chrome
 pyautogui.press('win')
 pyautogui.write('chrome')
 pyautogui.press('enter')
+
+time.sleep(5)
+
     # 1.2 Entrar o link
+link = 'https://dlp.hashtagtreinamentos.com/python/intensivao/login'
+pyautogui.write(link)
+pyautogui.press('enter')
 
     # 1.3 Esperar o site carregar
-
+time.sleep(5)
 
 # Passo 2: Fazer login
+pyautogui.click(x=557, y=359) # campo de email
+pyautogui.write('aprendendopython@hotmail.com') 
+
+pyautogui.press('tab') # campo de senha
+pyautogui.write('python123456')
+
+pyautogui.press('tab') # botão de login
+pyautogui.press('enter')
+
+time.sleep(3)
+
 # Passo 3: Importar a base de dados dos produtos
-# Passo 4: Cadastrar um produto
+import pandas # biblioteca que lida com base de dados
+
+tabela = pandas.read_csv('produtos.csv')
+print(tabela)
+
+for linha in tabela.index:
+    # Passo 4: Cadastrar um produto
+    pyautogui.click(x=622, y=245)
+
+    codigo = tabela.loc[linha, 'codigo'] # Método 1
+    marca = tabela.loc[linha, 'marca']
+    tipo = tabela.loc[linha, 'tipo']
+    categoria = tabela.loc[linha, 'categoria']
+    
+    # Preencher os campos
+    pyautogui.write(str(codigo))
+    pyautogui.press('tab')
+    pyautogui.write(str(marca))
+    pyautogui.press('tab')
+    pyautogui.write(str(tipo))
+    pyautogui.press('tab')
+    pyautogui.write(str(categoria))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, 'preco_unitario'])) # Método 2
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, 'custo']))
+    pyautogui.press('tab')
+
+    obs = tabela.loc[linha, 'obs']
+    if not pandas.isna(obs):
+        pyautogui.write(str(obs))
+
+    pyautogui.press('tab') # Botão de enviar
+    pyautogui.press('enter')
+
+    pyautogui.scroll(5000)
+
 # Passo 5: Repetir o cadastro para todos os produtos
 
 # Bibliotecas
-  # instalar PyAutoGui --> abrir o terminal e digitar: pip install pyautogui
+  # instalar PyAutoGui
     # Abrir o terminal
     # Digitar: pip install pyautogui
     # importar no código: import pyautogui
+
+  # instalar Pandas -> abrir o terminal e digitar: pip install pandas numpy openpyxl
+    # Abrir o terminal
+    # Digitar: pip install pandas numpy openpyxl
+    # importar no código: import pandas
 
 
 
