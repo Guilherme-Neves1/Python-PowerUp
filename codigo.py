786705e600b9dab15ac5484b2bcf3a6a1ebdda90
Guilherme <guilherme.neves20@hotmail.com>
2023-09-28 12:26:15 -0300

    Aula 01 do Intensivão de Python da Hashtag

---
+++ codigo.py
@@ -0,0 +1,38 @@
+# Passo a passo do projeto
+
+# Importando a biblioteca
+import pyautogui
+
+pyautogui.PAUSE = 0.5 # Pausa entre cada comando
+
+# Comandos
+# pyautogui.click -> clicar com o mouse 
+# pyautogui.write -> escrever um texto 
+# pyautogui.press -> apertar uma tecla 
+# pyautogui.hotkey -> atalho(combiação de teclas)
+
+
+# Passo 1: Entrar no sistema da empresa
+  # https://dlp.hashtagtreinamentos.com/python/intensivao/login
+    # 1.1 Abrir o Chrome
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+    # 1.2 Entrar o link
+
+    # 1.3 Esperar o site carregar
+
+
+# Passo 2: Fazer login
+# Passo 3: Importar a base de dados dos produtos
+# Passo 4: Cadastrar um produto
+# Passo 5: Repetir o cadastro para todos os produtos
+
+# Bibliotecas
+  # instalar PyAutoGui --> abrir o terminal e digitar: pip install pyautogui
+    # Abrir o terminal
+    # Digitar: pip install pyautogui
+    # importar no código: import pyautogui
+
+
+
